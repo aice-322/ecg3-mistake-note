a7f6c850da9a66e5f557726b95993871a656596e
aice-322 <takuto322@icloud.com>
2026-09-06 23:49:30 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,35 +1,89 @@
-const CACHE = "ecg3-v2";
+const CACHE = "ecg3-v2.0.0";
 
 const ASSETS = [
+
   "./",
+
   "./index.html",
+
   "./manifest.webmanifest"
+
 ];
 
+/* インストール */
+
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+
   self.skipWaiting();
+
+  event.waitUntil(
+
+    caches.open(CACHE)
+
+      .then(cache => cache.addAll(ASSETS))
+
+  );
+
 });
+
+/* 古いキャッシュを削除 */
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    )
+
+    caches.keys()
+
+      .then(keys => {
+
+        return Promise.all(
+
+          keys
+
+            .filter(key => key !== CACHE)
+
+            .map(key => caches.delete(key))
+
+        );
+
+      })
+
+      .then(() => self.clients.claim())
+
   );
-  self.clients.claim();
+
 });
 
+/* 通信 */
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE)
+
+          .then(cache => {
+
+            cache.put(event.request, copy);
+
+          });
+
+        return response;
+
+      })
+
+      .catch(() => {
+
+        return caches.match(event.request);
+
+      })
+
   );
+
 });
